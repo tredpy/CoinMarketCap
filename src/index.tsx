781f22ff7e7f611668edf4bc2af55d85d30aa477
tred_py <murwoofmur@gmail.com>
2023-05-24 17:21:59 +1000

    add Header, SideBar, Button, AppLink, Loader, PageLoader, ThemeProvider, ThemeContext, ThemeSwitcher, useTheme

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import { createRoot } from 'react-dom/client';
-import { App } from 'app/App';
+import { ThemeProvider } from 'app/providers/ThemeProvider';
 import { BrowserRouter } from 'react-router-dom';
+import { App } from 'app/App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
     <BrowserRouter>
-        <App/>
+        <ThemeProvider>
+            <App/>
+        </ThemeProvider>
     </BrowserRouter>
 );
---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,29 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Link, LinkProps } from 'react-router-dom';
+import { FC } from 'react';
+import s from './AppLink.module.scss';
+
+export type AppLinkView = 'primary'
+interface AppLinkProps extends LinkProps {
+    className?: string
+    view?: AppLinkView
+}
+export const AppLink: FC<AppLinkProps> = (props) => {
+    const {
+        to,
+        className,
+        children,
+        view,
+        ...otherProps
+    } = props;
+
+    return (
+        <Link
+            to={to}
+            className={classNames(s.AppLink, {}, [className, s[view]])}
+            {...otherProps}
+        >
+            {children}
+        </Link>
+    );
+};
