ba20a365163b14dd96613f4bd0e51d9d0755f19d
tred_py <murwoofmur@gmail.com>
2023-05-24 14:22:51 +1000

    add TypeScript, WebpackDevServer, refactoring

---
+++ config/build/buildResolvers.ts
@@ -0,0 +1,13 @@
+import {ResolveOptions} from 'webpack';
+import {BuildOptions} from './types/config';
+
+export function buildResolvers({paths}: BuildOptions): ResolveOptions {
+    return {
+        extensions: ['.tsx', '.ts', '.js'],
+        preferAbsolute: true,
+        modules: [paths.src, 'node_modules'],
+        mainFiles: ['index'],
+        alias: {}
+    };
+}
+ 
