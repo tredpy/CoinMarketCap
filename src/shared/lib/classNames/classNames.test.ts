8feb95051934e817974c0573f8ad8aa764d1a1c4
tred_py <murwoofmur@gmail.com>
2023-05-24 15:35:05 +1000

    add Jest, classNames.test.ts

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,40 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+
+describe('classNames', () => {
+    test('With cls', () => {
+        expect(classNames('TEST')).toBe('TEST');
+    });
+
+    test('With additional', () => {
+        const expected = 'TEST class1 class2';
+        expect(classNames('TEST', {}, ['class1', 'class2']))
+            .toBe(expected);
+    });
+
+    test('With mods', () => {
+        const expected = 'TEST class1 class2 hovered scrollable';
+        expect(classNames(
+            'TEST',
+            { hovered: true, scrollable: true },
+            ['class1', 'class2']
+        )).toBe(expected);
+    });
+
+    test('With false mods', () => {
+        const expected = 'TEST class1 class2 hovered';
+        expect(classNames(
+            'TEST',
+            { hovered: true, scrollable: false },
+            ['class1', 'class2']
+        )).toBe(expected);
+    });
+
+    test('With undefined mods', () => {
+        const expected = 'TEST class1 class2 hovered';
+        expect(classNames(
+            'TEST',
+            { hovered: true, scrollable: undefined },
+            ['class1', 'class2']
+        )).toBe(expected);
+    });
+});
