4890b6edac9f174c59b5fd2298df6dda279ff18e
tred_py <murwoofmur@gmail.com>
2023-05-24 14:51:50 +1000

    add classNames.ts helper

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
+import { classNames } from 'shared/lib/classNames/classNames';
 import './styles/index.scss';
 
 export const App = () => {
-    return <div className="app">TEST</div>;
+    return <div className={classNames('app', {}, [])}>TEST</div>;
 };
