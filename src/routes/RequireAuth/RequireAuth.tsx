2b56eee8c53ccd3f0bc947bd5d6fc5cefe9dd270
tred_py <murwoofmur@gmail.com>
2023-06-14 11:59:49 +1000

    add RequireAuth, update AppRouter, add privates routes handler & fix build bugs

--- src/routes/RequireAuth/RequireAuth.tsx
+++ src/routes/RequireAuth/RequireAuth.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactElement } from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
 
@@ -6,7 +6,7 @@ import { getUserAuthData } from 'store/User';
 
 import { RoutePath } from '../RouteConfig/RouteConfig';
 
-export function RequireAuth ({ children }: { children: ReactNode }) {
+export function RequireAuth ({ children }: { children: ReactElement }) {
     const auth = useSelector(getUserAuthData);
     const location = useLocation();
 
